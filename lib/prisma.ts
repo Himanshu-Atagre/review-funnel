dcd009d33cd42051fbb169a87f5105bd76e7c197
Himanshu <atagreh@gmail.com>
2026-05-09 17:53:07 +0530

    fixed prisma import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({} as any)
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
